642219778813d8f827c1b08867de5478f5feb53c
Artur23041989 <danh1989@mail.ru>
2025-05-22 21:32:21 +0300

    new comit

--- shop/urls_staff.py
+++ shop/urls_staff.py
@@ -1,8 +1,15 @@
 from django.urls import path
-from .views import CategoryCreatedView, CategoryListView
+from .views import (CategoryCreatedView, CategoryListView,
+                    ProductListView, ProductCreatedView,
+                    ProductDetailView)
+
 
 app_name = 'staff'
 urlpatterns = [
     path('categories/add/', CategoryCreatedView.as_view(), name='category_add'),
-    path('categories', CategoryListView.as_view(), name='categories'),
+    path('categories/', CategoryListView.as_view(), name='categories'),
+
+    path('products/add/', ProductCreatedView.as_view(), name='product_add'),
+    path('products/<slug:slug>/', ProductDetailView.as_view(), name='product_detail'),
+    path('products/', ProductListView.as_view(), name='products'),
 ]
